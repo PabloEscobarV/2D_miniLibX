828c2aa3406372c8a2aec6baa50de145a03f9e70
Pablo Escobar <sataniv.rider@gmail.com>
2024-04-02 23:25:06 +0200

    finished beta mapmaked func

--- hdrs/so_long.h
+++ hdrs/so_long.h
@@ -6,13 +6,23 @@
 /*   By: blackrider <blackrider@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 20:56:21 by blackrider        #+#    #+#             */
-/*   Updated: 2024/04/01 21:44:05 by blackrider       ###   ########.fr       */
+/*   Updated: 2024/04/02 23:10:23 by blackrider       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
 
-typedef unsigned char uchar;
+#define	HEX_FDF "0123456789ABCDEF"
+
+typedef unsigned char t_uchr;
+
+typedef	struct s_map
+{
+	int		x;
+	int		y;
+	int		altitude;
+	long	color;
+}			t_map;
 
 typedef	struct s_mlximg
 {
--- src/mapget.c
+++ src/mapget.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mapget.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: polenyc <polenyc@student.42.fr>            +#+  +:+       +#+        */
+/*   By: blackrider <blackrider@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 20:55:16 by blackrider        #+#    #+#             */
-/*   Updated: 2024/04/02 14:58:42 by polenyc          ###   ########.fr       */
+/*   Updated: 2024/04/02 23:21:46 by blackrider       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,27 @@ void	*freematrix(void **ptr)
 	return (NULL);
 }
 
+void	*freetriple(void ***ptr)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (ptr[i])
+	{
+		j = 0;
+		while (ptr[i][j])
+		{
+			free(ptr[i][j]);
+			++j;
+		}
+		free(ptr[i]);
+		++i;
+	}
+	free(ptr);
+	return (NULL);
+}
+
 int		sizeptr(char **map)
 {
 	int	size;
@@ -57,23 +78,17 @@ int		sizeptr(char **map)
 
 char	**mapcopy(char *filename)
 {
+	size_t	size;
 	int		file;
 	char	*data;
-	char	*tmp;
 	char	**map;
 
+	size = ft_filesize(filename);
 	file = open(filename, O_RDONLY);
-	if (file < 0)
+	data = malloc((size + 1) * sizeof(char));
+	if (file < 0 || !data)
 		return (NULL);
-	data = NULL;
-	tmp = get_next_line(file);
-	while (tmp)
-	{
-		data = ft_strjoinfree(data, tmp, 2);
-		if (!data)
-			return ((char **)return_free(data, file));
-		tmp = get_next_line(file);
-	}
+	read(file, data, size);
 	map = ft_split(data, '\n');
 	free(data);
 	close(file);
@@ -92,6 +107,7 @@ char	***maptostr(char *filename)
 	map = malloc((size + 1) * sizeof(char **));
 	if (!map)
 		return (NULL);
+	i = 0;
 	while(tmp[i])
 	{
 		map[i] = ft_split(tmp[i], ' ');
@@ -102,50 +118,165 @@ char	***maptostr(char *filename)
 	return (map);
 }
 
+size_t		*sizematrix(char ***map)
+{
+	size_t	*size;
 
+	size = ft_calloc(3, sizeof(size_t));
+	if (!size || !map)
+		return (NULL);
+	while (map[0][size[1]])
+		++(size[1]);
+	while (map[size[0]])
+		++(size[0]);
+	size[2] = 2;
+	return (size);
+}
 
-// int		**mapmatrix(char *filename)
-// {
-// 	char	***strmap;
-// 	char	***tmp;
-// 	int		**map;
+int		***createintmap(char ***str)
+{
+	int		i;
+	size_t	*sizemap;
+	int		***map;
 
-// 	strmap = maptostr(filename);
-// 	if (!strmap)
-// 		return (NULL);
-// 	tmp = strmap;
-// 	while (*tmp)
-// 	{
+	sizemap = sizematrix(str);
+	map = ft_calloc(sizemap[0] + 1, sizeof(int **));
+	if (!map)
+		return (NULL);
+	while ((sizemap[0])--)
+	{
+		map[sizemap[0]] = ft_calloc(sizemap[1] + 1, sizeof(int *));
+		i = 0;
+		while (i < sizemap[1])
+		{
+			map[sizemap[0]][i] = ft_calloc(sizemap[3], sizeof(int));
+			++i;
+		}
+	}
+	return (map);
+}
 
-// 	}
-// 	return ();
-// }
+t_map		*crtt_map(int x, int y, int alt, long color)
+{
+	t_map	*tmp;
+
+	tmp = malloc(sizeof(t_map));
+	if (!tmp)
+		return (NULL);
+	tmp->altitude = alt;
+	tmp->color = color;
+	tmp->x = x;
+	tmp->y =y;
+	return (tmp);
+}
+
+t_map	*sett_map(t_map *data, char *str, int x, int y)
+{
+	char	**strdata;
+	char	**tmp;
+
+	if (!data)
+		data = crtt_map(0, 0, 0, 0);
+	if (!data)
+		return (NULL);
+	tmp = ft_split(str, ',');
+	strdata = tmp;
+	data->x = x;
+	data->y = y;
+	data->altitude = ft_atoi(*tmp);
+	if (!(*(tmp + 1)))
+	{
+		freematrix((void **)tmp);
+		return (data);
+	}
+	if (**(tmp + 1) == '0' && !ft_isdigit(*(*(tmp + 1) + 1)))
+		data->color = ft_atoi_base(*(tmp + 1), HEX_FDF);
+	else
+		data->color = ft_atoi(*(tmp + 1));
+	freematrix((void **)tmp);
+	return (data);
+}
+
+t_llist		*crtlstmap(t_llist **map, char ***str)
+{
+	t_map	*tmp;
+	int		x;
+	int		y;
+
+	y = 0;
+	while (str[y])
+	{
+		x = 0;
+		while (str[y][x])
+		{
+			tmp = sett_map(NULL, str[y][x], x, y);
+			llistadd_back(map, llistnewnode(tmp));
+			++x;
+		}
+		++y;
+	}
+	return (*map);
+}
+
+t_llist		*maptollst(char *filename)
+{
+	char	***str;
+	t_llist	*map;
+
+	str = maptostr(filename);
+	if (!str)
+		return (NULL);
+	map = NULL;
+	crtlstmap(&map, str);
+	freetriple((void ***)str);
+	return (map);
+}
+
+void	printdata(void *ptr)
+{
+	t_map	*data;
+
+	data = (t_map *)ptr;
+	printf("x: %d\ty: %d\taltitude: %d\tcolor: %d\n",
+		data->x, data->y, data->altitude, data->color);
+}
+
+void	delfunc(void *data)
+{
+	free(data);
+}
 
 int	main()
 {
+	t_llist	*map;
+	size_t	*size;
 	char	filename[1024];
 	char	***data;
 
 	printf("Enter the filename:\n");
 	scanf("%s", filename);
-	data = maptostr(filename);
-	if (!data)
-	{
-		perror("Bad file operation");
-		exit(-1);
-	}
-	while (*data)
-	{
-		while (**data)
-		{
-			printf("%s", **data);
-			printf("|");
-			++(*data);
-		}
-		printf("\n");
-		++data;
-	}
-	printf("\n");
+	// data = maptostr(filename);
+	// if (!data)
+	// {
+	// 	perror("Bad file operation");
+	// 	exit(-1);
+	// }
+	// size = sizematrix(data);
+	// printf("rows: %d\tcolums: %d\tcount data: %d\n", size[0], size[1], size[2]);
+	// while (*data)
+	// {
+	// 	while (**data)
+	// 	{
+	// 		printf("%s", **data);
+	// 		printf("\t");
+	// 		++(*data);
+	// 	}
+	// 	printf("\n");
+	// 	++data;
+	// }
+	map = maptollst(filename);
+	llistiter(map, printdata);
+	llistclear(&map, delfunc);
 	return (0);
 }
 
